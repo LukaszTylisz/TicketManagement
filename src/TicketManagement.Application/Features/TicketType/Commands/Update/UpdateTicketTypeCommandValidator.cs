04ebb7a42e0c88e25df84af781aad46b62cdeb21
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-01-10 14:27:22 +0100

    Updated packages

--- src/TicketManagement.Application/Features/TicketType/Commands/Update/UpdateTicketTypeCommandValidator.cs
+++ src/TicketManagement.Application/Features/TicketType/Commands/Update/UpdateTicketTypeCommandValidator.cs
@@ -21,7 +21,7 @@ public class UpdateTicketTypeCommandValidator : AbstractValidator<UpdateTicketTy
         RuleFor(p => p.DefaultDays)
             .LessThan(100).WithMessage("{PropertyName} cannot exceed 100")
             .GreaterThan(1).WithMessage("{PropertyName} cannot be less than 1");
-        
+
         this._ticketTypeRepository = ticketTypeRepository;
     }
 
